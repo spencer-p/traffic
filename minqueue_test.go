f103e4b51255d7b474ee9f431c22906f82026c3f
Spencer Peterson <spencerjpeterson@gmail.com>
2017-12-07 13:57:33 -0800

    fix minqueue tests not failing when they should

--- minqueue_test.go
+++ minqueue_test.go
@@ -5,7 +5,41 @@ import (
 	"testing"
 )
 
-func TestMinQueue(t *testing.T) {
+func ExampleMinQueue() {
+	// New queue
+	q := newMinQueue(5)
+
+	// Initialize nodes named 0-4
+	var nodes [5]Node
+	for i := 0; i < len(nodes); i++ {
+		nodes[i] = &node{name: fmt.Sprint(i)}
+		q.push(nodes[i])
+	}
+
+	// Give em some priorities
+	q.update(nodes[0], 3.14)
+	q.update(nodes[1], 0.2)
+	q.update(nodes[2], 1)
+	q.update(nodes[3], 0.1)
+
+	// Modify a priority
+	q.update(nodes[0], 0.5)
+
+	for q.Len() > 0 {
+		n := q.pop()
+		_ = n
+		fmt.Println(n.Name())
+	}
+
+	// Output:
+	// 3
+	// 1
+	// 0
+	// 2
+	// 4
+}
+
+func TestMinQueueSetup(t *testing.T) {
 	// New queue
 	q := newMinQueue(5)
 
@@ -23,35 +57,5 @@ func TestMinQueue(t *testing.T) {
 		if len(q.lookup) != 0 {
 			t.Fail()
 		}
-	})
-
-	// Initialize nodes named 0-4
-	var nodes [5]Node
-	for i := 0; i < len(nodes); i++ {
-		nodes[i] = &node{name: fmt.Sprint(i)}
-		q.push(nodes[i])
-	}
-
-	t.Run("priority updates work", func(t *testing.T) {
-		// Give em some priorities
-		q.update(nodes[0], 3.14)
-		q.update(nodes[1], 0.2)
-		q.update(nodes[2], 1)
-		q.update(nodes[3], 0.1)
-
-		// Modify a priority
-		q.update(nodes[0], 0.5)
-
-		for q.Len() > 0 {
-			n := q.pop()
-			fmt.Println(n.Name())
-		}
-
-		// Output:
-		// 3
-		// 1
-		// 0
-		// 2
-		// 4
 	})
 }
